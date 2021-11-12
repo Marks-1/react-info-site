9dde62ad10077ab1f7274f22bb171224e4535de4
onyachmark <onyachmark@gmail.com>
2021-11-12 10:59:15 +0300

    commit these

---
+++ src/components/MainContent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function MainContent() {
+    return (
+        <main>
+            <h1>Heading one for the main content</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit deserunt quasi veniam ad dolor obcaecati, accusantium sunt nesciunt voluptates dicta at id laudantium! Laborum, eveniet. Cumque voluptatum consectetur dolor reiciendis.</p>
+        </main>
+    )
+}
+
+export default MainContent
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <nav>
+            <img src="" alt="Logo" />
+            <ul>
+                <li>Home</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
